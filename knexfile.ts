cb4cd15c93fd82ca8d3520815831c94cb1867659
Ah Ho <shumkwanho@gmail.com>
2024-05-20 15:19:57 +0800

    basic setup

---
+++ knexfile.ts
@@ -0,0 +1,57 @@
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "storybook",
+      user: "postgres",
+      password: "postgres",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+    debug:true
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "storybook",
+      user: "postgres",
+      password: "postgres"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "storybook",
+      user: "postgres",
+      password: "postgres"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+
+};
+
+module.exports = config;
--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-console.log("hi");
+import express from "express";
+import { Request, Response } from "express";
+import expressSession from "express-session";
+const app = express();
+const PORT = 8080;
+
+app.use(
+    expressSession({
+        secret: "Tecky Academy teaches typescript",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+declare module "express-session" {
+    interface SessionData {
+        username?: string;
+    }
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static("public"));
+app.use((req: Request, res: Response) => {
+    res.status(404).json({ "Message": "404 NOT FOUND" })
+})
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}/`);
+});
