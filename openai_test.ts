14bf30f6a025f4055bd8408e7b811a3a79254398
billsfyuen <billsfyuen@gmail.com>
2024-05-21 12:48:15 +0800

    added openai api key provided by adam

--- openai_test.ts
+++ openai_test.ts
@@ -1,16 +1,8 @@
-import OpenAI from 'openai';
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const openai = new OpenAI({
-  baseURL: process.env.TECKY_OPENAI_API_URL,
-  apiKey: process.env.OPENAI_API_KEY,
-});
+import { adamOpenAI, teckyOpenAI } from "./openai";
 
 //for testing chat model
 async function chatModel() {
-  const chatCompletion = await openai.chat.completions.create({
+  const chatCompletion = await teckyOpenAI.chat.completions.create({
     messages: [{ role: 'user', content: 'Say "Hello World" in English, Japanese, and Chinese' }],
     model: 'gpt-3.5-turbo',
   });
@@ -19,3 +11,18 @@ async function chatModel() {
 }
 
 chatModel();
+
+//for testing image model
+async function imageModel() {
+  const response = await adamOpenAI.images.generate({
+    model: "dall-e-2",
+    prompt: "a happy dog",
+    n: 1,
+    size: "1024x1024",
+  });
+  let image_url = response.data[0].url;
+
+  console.log(image_url);
+}
+
+// imageModel()
