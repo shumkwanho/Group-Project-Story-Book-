06897e451bd5b15b8148512135d356d8507467c3
billsfyuen <billsfyuen@gmail.com>
2024-05-22 16:41:57 +0800

    update router.ts

--- router.ts
+++ router.ts
@@ -2,15 +2,18 @@ import { Router } from "express"
 import { knex } from "./utils/knex"
 import { payment,webhook } from "./utils/payment";
 import  express from "express";
+
 import { CharacterService } from "./service/characterService";
 import { CharacterController } from "./controller/characterController";
+
 import { CommentService } from "./service/commentService";
 import { CommentController } from "./controller/commentController";
+
 import { PageService } from "./service/pageService";
 import { PageController } from "./controller/pageController";
+
 import { StorybookService } from "./service/storybookService";
 import { StorybookController } from "./controller/storybookController";
-
 
 export const router = Router()
 
@@ -20,18 +23,6 @@ router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
 router.post('/create-checkout-session', payment); 
-
-
-
-
-
-
-router.post('/webhook', express.raw({type: 'application/json'}), webhook);
-
-router.use(express.urlencoded({ extended: true }));
-router.use(express.json());
-router.post('/create-checkout-session', payment); 
-
 
 const characterService = new CharacterService(knex)
 const characterController = new CharacterController (characterService)
@@ -46,10 +37,11 @@ router.get("/comment",commentController.getAllComment);
 router.post("/comment",commentController.createComment);
 router.put("/comment",commentController.updateComment);
 router.delete("/comment",commentController.deleteComment);
+
 const storybookService = new StorybookService(knex)
 const storybookController = new StorybookController(storybookService)
-
 router.get("/storybook",storybookController.getAllStoryBook)
+
 const pageService = new PageService(knex);
 const pageController = new PageController(pageService);
 router.get('/page', pageController.getPageByStorybookId);
