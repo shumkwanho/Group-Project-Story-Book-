e46340abf5977b317c582b5ea778ebfbfe355ad0
Ziner Fung <siner1987s@gmail.com>
2024-05-22 14:06:44 +0800

    commit

--- main.ts
+++ main.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { Request, Response } from "express";
 import expressSession from "express-session";
 import { router } from "./router";
-import loginRoute from './loginRoute';
-import userRoute from './userRoute';
+import loginRoute from './routes/loginRoute';
+import userRoute from './routes/userRoute';
+import bodyParser from 'body-parser';
+import path from 'path';
 
 const app = express();
 const PORT = 8080;
@@ -22,16 +24,25 @@ declare module "express-session" {
     }
 }
 
-app.use("/login", express.static("public/login"))
-
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('/api', loginRoute);
-app.use('/api', userRoute);
-app.use("/",router)
+app.use('/user', userRoute);
+app.use("/",router);
 
+
+app.use("/login", express.static("public/login"))
 app.use(express.static("public"));
 app.use((req: Request, res: Response) => {
     res.status(404).json({ "Message": "404 NOT FOUND" })
 })
+
+app.get('/text.html', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, 'text.html'));
+  });
+
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}/`);
 });
