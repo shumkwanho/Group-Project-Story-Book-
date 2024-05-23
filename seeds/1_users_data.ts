6c3f6a7bda5ab83479091725282d150f995ed6b6
Ziner Fung <siner1987s@gmail.com>
2024-05-23 11:17:55 +0800

    commit

--- seeds/1_users_data.ts
+++ seeds/1_users_data.ts
@@ -6,8 +6,9 @@ export async function seed(knex: Knex): Promise<void> {
 
     // Inserts seed entries
     await knex("users").insert([
-        { username: "amy1234" ,email: "amy1234@gmail.com", password:"1234"},
-        { username: "peter5678" ,email: "peter5678@gmail.com", password:"1234" },
-        { username: "bob1314" ,email: "bob1314@gmail.com", password:"1234" }
+        //hash password for '123'
+        { username: "amy1234" ,email: "amy1234@gmail.com", password:"$2a$10$7DU9J1GI2ZDgr/Aa7ZOi0.cdJwGYDNTg1OOeQBiesld9quR/Kjs3q"},
+        { username: "peter5678" ,email: "peter5678@gmail.com", password:"$2a$10$7DU9J1GI2ZDgr/Aa7ZOi0.cdJwGYDNTg1OOeQBiesld9quR/Kjs3q" },
+        { username: "bob1314" ,email: "bob1314@gmail.com", password:"$2a$10$7DU9J1GI2ZDgr/Aa7ZOi0.cdJwGYDNTg1OOeQBiesld9quR/Kjs3q" }
     ]);
 };
