9d967340e240d48ee6fd27226139a2e1ceaf0117
Ziner Fung <siner1987s@gmail.com>
2024-05-30 10:31:37 +0800

    fixed merge error

--- public/mainPage/index.js
+++ public/mainPage/index.js
@@ -35,18 +35,6 @@ window.addEventListener("load", async (e) => {
 //             </div>`
 //     }
 // }
-// const loadCharacters = async () => {
-//     const res = await fetch("/characters")
-//     const data = (await res.json()).data
-//     const characterArea = document.querySelector(".character-area")
-//     for (let character of data) {
-//         characterArea.innerHTML +=
-//             `<div class="character border" id="character_${character.id}" onclick="showCharacterCard(${character.id})">
-//                 <div class="character-image">image</div>
-//                 <div class="character-name">${character.name}</div>
-//             </div>`
-//     }
-// }
 
 const loadStorybooks = (data) => {
     const storybookArea = document.querySelector(".storybook-area")
