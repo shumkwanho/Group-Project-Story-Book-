3d19595f4898d318c7955668bc7bc474c52f4a16
billsfyuen <billsfyuen@gmail.com>
2024-07-25 09:32:45 +0800

    testing commit

--- public/book/index.js
+++ public/book/index.js
@@ -2,10 +2,15 @@ import { bookReader } from "../helpers/bookReader.js";
 import { login } from "../helpers/login.js";
 import { register } from "../helpers/register.js";
 import { getUserInfo } from "../helpers/auth.js";
-import { storybookUpdatePrivate, storybookUpdatePublic } from "../helpers/updateBookStatus.js";
+import {
+  storybookUpdatePrivate,
+  storybookUpdatePublic,
+} from "../helpers/updateBookStatus.js";
 
-const createComment = document.querySelector("#create-comment")
-const commentArea = document.querySelector(".comment-area")
+console.log("testing");
+
+const createComment = document.querySelector("#create-comment");
+const commentArea = document.querySelector(".comment-area");
 
 var searchParams = new URLSearchParams(window.location.search);
 const storybookId = searchParams.get("id");
@@ -16,63 +21,62 @@ window["storybookUpdatePublic"] = storybookUpdatePublic;
 window["editComment"] = editComment;
 window["deleteComment"] = deleteComment;
 window["confirmEdit"] = confirmEdit;
-window["login"] = login
-window["register"] = register
-window["logout"] = logout
-window["search"] = search
-window["toBookPage"] = toBookPage
+window["login"] = login;
+window["register"] = register;
+window["logout"] = logout;
+window["search"] = search;
+window["toBookPage"] = toBookPage;
 window["toggleLike"] = toggleLike;
 
 window.addEventListener("load", async (e) => {
-    const userId = await checkLogin()
+  const userId = await checkLogin();
 
-    const bookData = await getStoryBook(storybookId, userId)
-    await loadComment(storybookId)
-    
-    if (userId) {
-        await loadBtn()
-        await loadLike(bookData)
-    }
-})
+  const bookData = await getStoryBook(storybookId, userId);
+  await loadComment(storybookId);
+
+  if (userId) {
+    await loadBtn();
+    await loadLike(bookData);
+  }
+});
 
 const checkLogin = async () => {
-    const res = await fetch("/checkLogin")
-    const data = await res.json()
+  const res = await fetch("/checkLogin");
+  const data = await res.json();
 
-    if (data.data) {
+  if (data.data) {
+    const userInfo = await getUserInfo();
 
-        const userInfo = await getUserInfo()
+    document.querySelector("#username-display").innerHTML = userInfo.username;
 
-        document.querySelector("#username-display").innerHTML = userInfo.username;
+    document.querySelector("#user-page-redirect").classList.toggle("hide");
+    document.querySelector("#logout").classList.toggle("hide");
+    document.querySelector(".search-bar").addEventListener("input", search);
 
-        document.querySelector("#user-page-redirect").classList.toggle("hide")
-        document.querySelector("#logout").classList.toggle("hide")
-        document.querySelector(".search-bar").addEventListener("input", search)
+    document
+      .querySelector("#user-page-redirect")
+      .addEventListener("click", () => {
+        window.location.href = "../member";
+      });
 
-        document.querySelector("#user-page-redirect")
-            .addEventListener("click", () => {
-                window.location.href = '../member';
-            });
-        
-        return data.data
-    }
+    return data.data;
+  }
 
-    document.querySelector("#login").classList.toggle("hide")
-    document.querySelector("#register").classList.toggle("hide")
-    document.querySelector(".search-bar").addEventListener("input", search)
-    return null
-}
+  document.querySelector("#login").classList.toggle("hide");
+  document.querySelector("#register").classList.toggle("hide");
+  document.querySelector(".search-bar").addEventListener("input", search);
+  return null;
+};
 
 async function getStoryBook(storybookId, userId) {
-    let res = await fetch(`/storybookById?id=${storybookId}`)
-    let data = (await res.json()).data
+  let res = await fetch(`/storybookById?id=${storybookId}`);
+  let data = (await res.json()).data;
 
-    if (res.ok) {
-
-        let target = document.querySelector(".upper-part");
-        target.insertAdjacentHTML(
-            "afterbegin",
-            `
+  if (res.ok) {
+    let target = document.querySelector(".upper-part");
+    target.insertAdjacentHTML(
+      "afterbegin",
+      `
             <img src="../../uploads/pageImg/${data.image}" alt="" class="book-cover border">
             <div class="book-detail">
                 <div class="book-name">Book Name:  <h class="textcolor"> ${data.bookname}</h></div>
@@ -86,236 +90,234 @@ async function getStoryBook(storybookId, userId) {
             </button>
             </div>
             `
-        )
+    );
 
-        if (userId) {
-            if (data.is_public) {
-
-                document.querySelector(".book-button-group").insertAdjacentHTML(
-                    "beforeend",
-                    `
+    if (userId) {
+      if (data.is_public) {
+        document.querySelector(".book-button-group").insertAdjacentHTML(
+          "beforeend",
+          `
                 <button id="make-private" type="button" class="btn btn-primary btn-lg" data-bs-toggle="button" onclick="storybookUpdatePrivate(${storybookId})">
                 Make it Private</button>`
-                )
-
-            } else {
-
-                document.querySelector(".book-button-group").insertAdjacentHTML(
-                    "beforeend",
-                    `
+        );
+      } else {
+        document.querySelector(".book-button-group").insertAdjacentHTML(
+          "beforeend",
+          `
                 <button id="make-public" type="button" class="btn btn-primary btn-lg" data-bs-toggle="button" onclick="storybookUpdatePublic(${storybookId})">
                 Make it Public</button>`
-                )
-
-            }
-        }
-            return data
+        );
+      }
     }
+    return data;
+  }
 }
 
 async function loadLike(bookData) {
-    const likeRes = await fetch("../like")
-    const likeData = (await likeRes.json()).data.map(elem => elem.id)
-    const isLiked = likeData.includes(parseInt(storybookId))
-    document.querySelector(".function").innerHTML += `
+  const likeRes = await fetch("../like");
+  const likeData = (await likeRes.json()).data.map((elem) => elem.id);
+  const isLiked = likeData.includes(parseInt(storybookId));
+  document.querySelector(".function").innerHTML += `
     <div class="like-container">
-        <i class="fa-${isLiked ? "solid" : "regular"} fa-heart like-btn" style="color: #9ECDFF;" onclick=toggleLike(event,${storybookId})></i>
+        <i class="fa-${
+          isLiked ? "solid" : "regular"
+        } fa-heart like-btn" style="color: #9ECDFF;" onclick=toggleLike(event,${storybookId})></i>
         <span class="like-count">${bookData.likeCount}</span>
-    </div>`
+    </div>`;
 }
 
 async function toggleLike(e, bookId) {
-    e.stopPropagation()
-    e.target.classList.toggle('fa-regular')
-    e.target.classList.toggle('fa-solid')
-    const isLiked = e.target.classList.contains('fa-solid')
-    let likeCount = document.querySelector(".like-count")
-    if (isLiked) {
-        likeCount.innerHTML = parseInt(likeCount.innerHTML) + 1
-        const res = await fetch('../like', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-            },
-            body: JSON.stringify({ bookId }),
-        })
+  e.stopPropagation();
+  e.target.classList.toggle("fa-regular");
+  e.target.classList.toggle("fa-solid");
+  const isLiked = e.target.classList.contains("fa-solid");
+  let likeCount = document.querySelector(".like-count");
+  if (isLiked) {
+    likeCount.innerHTML = parseInt(likeCount.innerHTML) + 1;
+    const res = await fetch("../like", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({ bookId }),
+    });
 
-        return
-    }
-    likeCount.innerHTML = parseInt(likeCount.innerHTML) - 1
-    const res = await fetch('../dislike', {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({ bookId }),
-    })
-    return
+    return;
+  }
+  likeCount.innerHTML = parseInt(likeCount.innerHTML) - 1;
+  const res = await fetch("../dislike", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({ bookId }),
+  });
+  return;
 }
 
-
 const loadComment = async (storybookId) => {
-    const res = await fetch(`/comment?id=${storybookId}`)
-    const data = (await res.json()).data
-    for (let comment of data) {
-        const date = comment.updated_at.slice(0, 10)
-        commentArea.innerHTML += `
+  const res = await fetch(`/comment?id=${storybookId}`);
+  const data = (await res.json()).data;
+  for (let comment of data) {
+    const date = comment.updated_at.slice(0, 10);
+    commentArea.innerHTML += `
         <div class="comment-container" id="comment_${comment.id}">
             <div class="comment">
                 <div class="comment-detail">
-                    <div class="user">${comment.username ? comment.username : "Anonymous"}</div>
+                    <div class="user">${
+                      comment.username ? comment.username : "Anonymous"
+                    }</div>
                     <div class="comment-content">${comment.content}</div>
                 </div>
                 <div class="created-at">${date}</div>
             </div>
-        </div>`
-    }
-}
-
-
+        </div>`;
+  }
+};
 
 async function search(e) {
-    const searchResult = document.querySelector(".search-result-container")
-    searchResult.innerHTML = ""
-    const search = e.target.value
-    if (search.length == 0) {
-        searchResult.classList.add("hide")
-        return
-    }
-    searchResult.classList.remove("hide")
-    const res = await fetch('../search', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({ search }),
-    })
-    const data = (await res.json()).data
+  const searchResult = document.querySelector(".search-result-container");
+  searchResult.innerHTML = "";
+  const search = e.target.value;
+  if (search.length == 0) {
+    searchResult.classList.add("hide");
+    return;
+  }
+  searchResult.classList.remove("hide");
+  const res = await fetch("../search", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({ search }),
+  });
+  const data = (await res.json()).data;
 
-    for (let book of data) {
-        let bookname = book.bookname.replace(search, `<b>${search}</b>`)
+  for (let book of data) {
+    let bookname = book.bookname.replace(search, `<b>${search}</b>`);
 
-        searchResult.innerHTML += `
+    searchResult.innerHTML += `
         <div class="search-result border">
             <div class="book-detail" onclick="toBookPage(${book.id})">
                 <div class="search-bookname">${bookname}</div>
             </div>
             <img src="../../uploads/pageImg/${book.image}" alt="" class="search-image">
         </div>
-        `
-    }
+        `;
+  }
 }
 
 function toBookPage(bookId) {
-    window.location.href = `../book/?id=${bookId}`
+  window.location.href = `../book/?id=${bookId}`;
 }
 
 async function logout() {
-    const res = await fetch("/logout")
-    const data = await res.json()
-    window.location.reload()
+  const res = await fetch("/logout");
+  const data = await res.json();
+  window.location.reload();
 }
 
 createComment.addEventListener("click", async (e) => {
-    const newComment = document.querySelector("#new-comment").value
-    const res = await fetch(`/comment/?id=${storybookId}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({ newComment }),
-    })
-    if (res.ok) {
-        window.location.reload()
-    }
-})
+  const newComment = document.querySelector("#new-comment").value;
+  const res = await fetch(`/comment/?id=${storybookId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({ newComment }),
+  });
+  if (res.ok) {
+    window.location.reload();
+  }
+});
 
 async function loadBtn() {
-    const res = await fetch("/comment-user")
-    const data = (await res.json()).data
-    const commentIds = data.map(e => (e.id).toString())
+  const res = await fetch("/comment-user");
+  const data = (await res.json()).data;
+  const commentIds = data.map((e) => e.id.toString());
 
-    const comments = Array.from(document.querySelectorAll(".comment-container"))
-    for (let comment of comments){
-        const commentId = comment.id.slice(8)
-        if (!commentIds.includes(commentId)){
-           continue
-        }
-        document.querySelector(`#comment_${commentId}`).innerHTML += `
+  const comments = Array.from(document.querySelectorAll(".comment-container"));
+  for (let comment of comments) {
+    const commentId = comment.id.slice(8);
+    if (!commentIds.includes(commentId)) {
+      continue;
+    }
+    document.querySelector(`#comment_${commentId}`).innerHTML += `
         <div class="btn-group">
             <div id="editComment" onclick=editComment(${commentId})> <i class="fa-solid fa-pen" style="color: #48A0FF;"></i> </div>
             <div id="deleteComment"> <i onclick=deleteComment(event,${commentId}) class="fa-solid fa-trash" style="color: #48A0FF;"></i> </div>
         </div>
-        `
-    }
-
+        `;
+  }
 }
 
-async function deleteComment(e,commentId) {
-    const res = await fetch('/comment', {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({ commentId }),
-    })
-    const data = await res.json()
-    if (res.ok) {
+async function deleteComment(e, commentId) {
+  const res = await fetch("/comment", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({ commentId }),
+  });
+  const data = await res.json();
+  if (res.ok) {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
         Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire({
-                title: "Deleted!",
-                text: "Your comment has been deleted.",
-                icon: "success"
-              });
-              e.target.parentElement.parentElement.parentElement.remove()
-              window.location.reload()
-            }
-          });
-    }
+          title: "Deleted!",
+          text: "Your comment has been deleted.",
+          icon: "success",
+        });
+        e.target.parentElement.parentElement.parentElement.remove();
+        window.location.reload();
+      }
+    });
+  }
 }
 
 function editComment(commentId) {
-    const targetComment = document.querySelector(`#comment_${commentId}`)
-    const content = targetComment.querySelector(".comment-content").innerHTML
-    targetComment.innerHTML = `
+  const targetComment = document.querySelector(`#comment_${commentId}`);
+  const content = targetComment.querySelector(".comment-content").innerHTML;
+  targetComment.innerHTML = `
         <textarea class="edit-input" cols="72">${content}</textarea>
         <button type="button" onclick=confirmEdit(event,${commentId}) class="btn btn-primary">Confirm</button>
-    `
+    `;
 }
 
 async function confirmEdit(event, commentId) {
-    const comment = event.target.parentElement
-    const content = comment.querySelector("textarea").value
-    const res = await fetch('/comment', {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({ content, commentId }),
-    })
-    if (res.ok) {
-        window.location.reload()
-    }
+  const comment = event.target.parentElement;
+  const content = comment.querySelector("textarea").value;
+  const res = await fetch("/comment", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({ content, commentId }),
+  });
+  if (res.ok) {
+    window.location.reload();
+  }
 }
 
 document.addEventListener("click", (e) => {
-    const searchResult = document.querySelector(".search-result-container")
-    const searchBar = document.querySelector(".search-bar")
+  const searchResult = document.querySelector(".search-result-container");
+  const searchBar = document.querySelector(".search-bar");
 
-    let isClickTargetArea = document.activeElement === searchBar || document.activeElement === searchResult
-    let isDisplaying = !searchResult.classList.contains("hide")
+  let isClickTargetArea =
+    document.activeElement === searchBar ||
+    document.activeElement === searchResult;
+  let isDisplaying = !searchResult.classList.contains("hide");
 
-    if (isDisplaying && !isClickTargetArea) {
-        searchResult.classList.add("hide")
-        searchBar.value = ""
-    }
-})
+  if (isDisplaying && !isClickTargetArea) {
+    searchResult.classList.add("hide");
+    searchBar.value = "";
+  }
+});
