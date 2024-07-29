5f62f74a9d131379883fe54fe311ebe227d7ec6c
Bill Yuen <billsfyuen@gmail.com>
2024-07-29 14:37:42 +0800

    update image generator model in page controller

--- controller/pageController.ts
+++ controller/pageController.ts
@@ -5,6 +5,7 @@ import { Request, Response } from "express";
 import { genPageImagePrompt } from "../utils/promptGenerator";
 import { imageGeneratorModel, textGeneratorModel } from "../aiEngine/openaiGenerator";
 import { downloadImage } from "../utils/downloadImg";
+import { imageModelVII } from "../aiEngine/replicateGenerator";
 
 const TEXT_MODEL = 'gpt-3.5-turbo';
 const IMAGE_MODEL = 'dall-e-3'
@@ -12,7 +13,7 @@ const IMAGE_MODEL = 'dall-e-3'
 const characterService = new CharacterService(knex);
 
 export class PageController {
-    constructor (private pageService: PageService) {}
+    constructor(private pageService: PageService) { }
 
     getPageByStorybookId = async (req: Request, res: Response) => {
         try {
@@ -24,7 +25,7 @@ export class PageController {
                 message: "get page successful",
                 data: pageQueryResult
             })
-            
+
         } catch (error) {
             console.log(error);
             res.status(500).json({ message: "internal sever error" });
@@ -40,20 +41,25 @@ export class PageController {
 
             let characterRequirementJSON = JSON.parse(characterInfo[0].requirement);
             let storybookContentJSON = JSON.parse(storybookContentJSONStr)
-            
+
             let pageDetails = storybookContentJSON.scenario[pageNumber - 1]; // storybookContentJSON.scenario is array
 
             let pageTextPrompt = genPageImagePrompt(characterRequirementJSON, pageDetails);
             let pageTextPromptGPT = await textGeneratorModel(pageTextPrompt, TEXT_MODEL);
 
-            let pageImageURL = await imageGeneratorModel(pageTextPromptGPT as string, IMAGE_MODEL);
+            //using openai dall-e 3 model
+            // let pageImageURL = await imageGeneratorModel(pageTextPromptGPT as string, IMAGE_MODEL);
+
+            //using stable diffusion model from replicate
+            let pageImageURL = await imageModelVII(pageTextPromptGPT as string);
+
             let pageImageFileName = await downloadImage(pageImageURL as string, 'page');
 
             await this.pageService.createPage(
-                storybookId, 
-                pageDetails.description, 
-                pageImageFileName as string, 
-                pageNumber, 
+                storybookId,
+                pageDetails.description,
+                pageImageFileName as string,
+                pageNumber,
                 pageTextPromptGPT as string)
 
             res.json({
@@ -68,5 +74,5 @@ export class PageController {
             res.status(500).json({ message: "internal sever error" });
         }
     }
-    
+
 }
